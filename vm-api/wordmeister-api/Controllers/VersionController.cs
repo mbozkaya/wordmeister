fceb294de2760c96f66bf8f5213cb1700d256eed
muhammet.bozkaya <Muhammet.Bozkaya@sovos.com>
2020-10-09 11:45:47 +0300

    version

---
+++ vm-api/wordmeister-api/Controllers/VersionController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace wordmeister_api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class VersionController : ControllerBase
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        public string Index() => $"Welcome to WordMeister Api " +
+            $"Version:{GetType().Assembly.GetName().Version.ToString()} " +
+            $"© 2020 - {DateTime.Now.Year}";
+    }
+}
