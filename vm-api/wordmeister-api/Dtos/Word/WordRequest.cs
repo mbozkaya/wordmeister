822d377066cef05e93c0e324e58f190e4b5cdfa2
yelqan <ozmustafa94@gmail.com>
2021-01-31 20:41:17 +0300

    dtos are changed for word crud operations

---
+++ vm-api/wordmeister-api/Dtos/Word/WordRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace wordmeister_api.Dtos.Word
+{
+    public class WordRequest
+    {
+        public long Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ vm-api/wordmeister-api/Dtos/Word/WordResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace wordmeister_api.Dtos.Word
+{
+    public class WordResponse
+    {
+        public long Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+        public ICollection<SentenceDto> Sentences { get; set; }
+    }
+}
